e3bb909cd7230ff93d39a1889fe1b94a292af689
dbdgjthgcbcc <85731247+dbdgjthgcbcc@users.noreply.github.com>
2022-05-10 18:56:53 +1200

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -7,6 +7,8 @@ var firebaseConfig = {
       messagingSenderId: "350314592904",
       appId: "1:350314592904:web:84a037fb2ea7f8b99efd3d"
 };
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 function getData() {
@@ -15,9 +17,30 @@ function getData() {
                   childKey = childSnapshot.key;
                   Room_names = childKey;
                   //Start code
-
+                  console.log("Room Name - " + Room_names);
+                  row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+                  document.getElementById("output").innerHTML += row;
                   //End code
             });
       });
 }
 getData();
+
+function addRoom() {
+      room_name = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(room_name).update({
+            purpose: "adding room name"
+      });
+
+      localStorage, setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+
+}
+
+function redirectToRoomName(name) {
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
